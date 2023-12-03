138010e6593e6a1ec1636381507940b6ba20f8e2
nitemoss <uhinemine@gmail.com>
2023-12-03 23:47:49 +0200

    Initial commit
    Update az22088_main.py

---
+++ az22088_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from az22088_libadd import *
+from az22088_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
